90138bde30a8b8d5e93884281ddcf62825f30d94
steelthedev <akinwumikaliyanu@gmail.com>
2021-06-21 15:27:41 +0100

    "

--- scrape.py
+++ scrape.py
@@ -1,14 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 
-source=requests.get('http://thenetnaija.com').text
+source=requests.get('https://www.thenetnaija.com/videos/tag/Horror').text
 
 soup=BeautifulSoup(source,'lxml')
 
-content_main=soup.find('div',class_="file-one-shadow")
+content_main=soup.find('div',class_="video-files")
 
-#print(content_main.prettify())
 
-title=content_main.a
+articles =content_main.find_all('article')
 
-print(title)
+for article in articles:
+
+    article_image=article.find('div', class_="thumbnail").img['src']
+
+    article_info=article.find('div',class_="info")
+
+    article_link=article_info.h2.a['href']
+
+    article_name=article_info.h2.a.text
+
+    print(article_image)
+
+
+    print(article_link)
+
+    print(article_name)
+
+    print()
